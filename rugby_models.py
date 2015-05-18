ce6b86a436ed308e988d9af4fab29ef0cea3de4e
Trevor <trceniviva@gmail.com>
2015-05-18 18:33:07 -0400

    general update

--- rugby_models.py
+++ rugby_models.py
@@ -20,15 +20,15 @@ rugby = pd.read_table('rugbyfinal.csv', sep=',')
 
 del rugby['game']
 
+feature_cols = ['home','avgPD','avgPD_opp','wins','wins_opp']
+
 full_cols = []
 
-feature_cols = ['home','avgPD','avgPD_opp']
+x_tr18 = rugby[rugby.round < 20][rugby.round > 1][feature_cols]
+y_tr18 = rugby[rugby.round < 20][rugby.round > 1].win
 
-x_tr18 = rugby[rugby.round < 18][rugby.round > 1][feature_cols]
-y_tr18 = rugby[rugby.round < 18][rugby.round > 1].win
-
-x_t18 = rugby[rugby.round == 18][feature_cols]
-y_t18 = rugby[rugby.round == 18].win
+x_t18 = rugby[rugby.round > 19][feature_cols]
+y_t18 = rugby[rugby.round > 19].win
 
 logreg.fit(x_tr18, y_tr18)
 
@@ -36,53 +36,7 @@ y_p18 = logreg.predict(x_t18)
 
 full_cols.append(metrics.accuracy_score(y_t18,y_p18))
 
-###################################################################
-###################################################################
-###################################################################
-
-x_tr19 = rugby[rugby.round < 19][rugby.round > 1][feature_cols]
-y_tr19 = rugby[rugby.round < 19][rugby.round > 1].win
-
-x_t19 = rugby[rugby.round == 19][feature_cols]
-y_t19 = rugby[rugby.round == 19].win
-
-logreg.fit(x_tr19, y_tr19)
-
-y_p19 = logreg.predict(x_t19)
-
-full_cols.append(metrics.accuracy_score(y_t19,y_p19))
-
-###################################################################
-###################################################################
-###################################################################
-
-x_tr20 = rugby[rugby.round < 20][rugby.round > 1][feature_cols]
-y_tr20 = rugby[rugby.round < 20][rugby.round > 1].win
-
-x_t20 = rugby[rugby.round == 20][feature_cols]
-y_t20 = rugby[rugby.round == 20].win
-
-logreg.fit(x_tr20, y_tr20)
-
-y_p20 = logreg.predict(x_t20)
-
-full_cols.append(metrics.accuracy_score(y_t20,y_p20))
-
-###################################################################
-###################################################################
-###################################################################
-
-x_tr21 = rugby[rugby.round < 21][rugby.round > 1][feature_cols]
-y_tr21 = rugby[rugby.round < 21][rugby.round > 1].win
-
-x_t21 = rugby[rugby.round == 21][feature_cols]
-y_t21 = rugby[rugby.round == 21].win
-
-logreg.fit(x_tr21, y_tr21)
-
-y_p21 = logreg.predict(x_t21)
-
-full_cols.append(metrics.accuracy_score(y_t21,y_p21))
+full_cols
 
 ###################################################################
 ###################################################################
@@ -90,13 +44,13 @@ full_cols.append(metrics.accuracy_score(y_t21,y_p21))
 
 just_wins = []
 
-feature_cols = ['wins','wins_opp']
+feature_cols = ['home','wins','wins_opp']
 
-x_tr18 = rugby[rugby.round < 18][rugby.round > 1][feature_cols]
-y_tr18 = rugby[rugby.round < 18][rugby.round > 1].win
+x_tr18 = rugby[rugby.round < 20][rugby.round > 1][feature_cols]
+y_tr18 = rugby[rugby.round < 20][rugby.round > 1].win
 
-x_t18 = rugby[rugby.round == 18][feature_cols]
-y_t18 = rugby[rugby.round == 18].win
+x_t18 = rugby[rugby.round > 19][feature_cols]
+y_t18 = rugby[rugby.round > 19].win
 
 logreg.fit(x_tr18, y_tr18)
 
@@ -104,124 +58,35 @@ y_p18 = logreg.predict(x_t18)
 
 just_wins.append(metrics.accuracy_score(y_t18,y_p18))
 
-###################################################################
-###################################################################
-###################################################################
-
-x_tr19 = rugby[rugby.round < 19][rugby.round > 1][feature_cols]
-y_tr19 = rugby[rugby.round < 19][rugby.round > 1].win
-
-x_t19 = rugby[rugby.round == 19][feature_cols]
-y_t19 = rugby[rugby.round == 19].win
-
-logreg.fit(x_tr19, y_tr19)
-
-y_p19 = logreg.predict(x_t19)
-
-just_wins.append(metrics.accuracy_score(y_t19,y_p19))
+just_wins
 
 ###################################################################
 ###################################################################
 ###################################################################
 
-x_tr20 = rugby[rugby.round < 20][rugby.round > 1][feature_cols]
-y_tr20 = rugby[rugby.round < 20][rugby.round > 1].win
+predict = []
 
-x_t20 = rugby[rugby.round == 20][feature_cols]
-y_t20 = rugby[rugby.round == 20].win
+feature_cols = ['home']
 
-logreg.fit(x_tr20, y_tr20)
+x_tr18 = rugby[rugby.round < 20][rugby.round > 1][feature_cols]
+y_tr18 = rugby[rugby.round < 20][rugby.round > 1].win
 
-y_p20 = logreg.predict(x_t20)
-
-just_wins.append(metrics.accuracy_score(y_t20,y_p20))
-
-###################################################################
-###################################################################
-###################################################################
-
-x_tr21 = rugby[rugby.round < 21][rugby.round > 1][feature_cols]
-y_tr21 = rugby[rugby.round < 21][rugby.round > 1].win
-
-x_t21 = rugby[rugby.round == 21][feature_cols]
-y_t21 = rugby[rugby.round == 21].win
-
-logreg.fit(x_tr21, y_tr21)
-
-y_p21 = logreg.predict(x_t21)
-
-just_wins.append(metrics.accuracy_score(y_t21,y_p21))
-
-###################################################################
-###################################################################
-###################################################################
-
-justpds = []
-
-feature_cols = ['avgPD','avgPD_opp']
-
-x_tr18 = rugby[rugby.round < 18][rugby.round > 1][feature_cols]
-y_tr18 = rugby[rugby.round < 18][rugby.round > 1].win
-
-x_t18 = rugby[rugby.round == 18][feature_cols]
-y_t18 = rugby[rugby.round == 18].win
+x_t18 = rugby[rugby.round > 1][feature_cols]
+y_t18 = rugby[rugby.round > 1].win
 
 logreg.fit(x_tr18, y_tr18)
 
 y_p18 = logreg.predict(x_t18)
 
-justpds.append(metrics.accuracy_score(y_t18,y_p18))
+predict.append(metrics.accuracy_score(y_t18,y_p18))
+
+predict
 
 ###################################################################
 ###################################################################
 ###################################################################
 
-x_tr19 = rugby[rugby.round < 19][rugby.round > 1][feature_cols]
-y_tr19 = rugby[rugby.round < 19][rugby.round > 1].win
-
-x_t19 = rugby[rugby.round == 19][feature_cols]
-y_t19 = rugby[rugby.round == 19].win
-
-logreg.fit(x_tr19, y_tr19)
-
-y_p19 = logreg.predict(x_t19)
-
-justpds.append(metrics.accuracy_score(y_t19,y_p19))
-
-###################################################################
-###################################################################
-###################################################################
-
-x_tr20 = rugby[rugby.round < 20][rugby.round > 1][feature_cols]
-y_tr20 = rugby[rugby.round < 20][rugby.round > 1].win
-
-x_t20 = rugby[rugby.round == 20][feature_cols]
-y_t20 = rugby[rugby.round == 20].win
-
-logreg.fit(x_tr20, y_tr20)
-
-y_p20 = logreg.predict(x_t20)
-
-justpds.append(metrics.accuracy_score(y_t20,y_p20))
-
-###################################################################
-###################################################################
-###################################################################
-
-x_tr21 = rugby[rugby.round < 21][rugby.round > 1][feature_cols]
-y_tr21 = rugby[rugby.round < 21][rugby.round > 1].win
-
-x_t21 = rugby[rugby.round == 21][feature_cols]
-y_t21 = rugby[rugby.round == 21].win
-
-logreg.fit(x_tr21, y_tr21)
-
-y_p21 = logreg.predict(x_t21)
-
-justpds.append(metrics.accuracy_score(y_t21,y_p21))
-
-
-sum(full_cols)/4
-sum(just_wins)/4
-sum(justpds)/4
+full_cols
+just_wins
+justhome
 
